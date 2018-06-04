6562519f1f4d2b079995407c8c526fcac0c1c1ed
AwsomeCraft126 <AwsomeCraft@icloud.com>
2018-06-04 14:36:39 -0400

    Add files via upload

---
+++ src/BossFly/Main.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AwsomeCraft126
+ * Date: 6/2/2018
+ * Time: 3:21 PM
+ */
+
+namespace BossFly;
+
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+
+
+class BossFly extends PluginBase implements Listener[
+    Public function onEnable()[
+        $this->getLogger()->info("BossFly Enabled");
+]
+
+    Public function onDisable()[
+        $this->getLogger()->info("BossFly Disabled");
+]
+]
